51296b59e638a44a16526935e827349479cc1658
Leander Volprich <l.volprich@buero-digitale.de>
2024-03-28 11:55:03 +0100

    Todo App initialize

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css',
+})
+export class TodoComponent implements OnInit {
+  @Input() todo = { todo: '', done: false };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
