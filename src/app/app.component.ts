d32e09f227da2c6802ddb6c27ce048b1712159f3
Leander Volprich <l.volprich@buero-digitale.de>
2024-03-28 14:04:46 +0100

    todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,30 @@ export class AppComponent {
     { todo: 'Programmieren', done: true },
   ];
 
+  newTodo = '';
+
+  setTodo(event: KeyboardEvent) {
+    this.newTodo = (event.target as HTMLInputElement).value;
+    console.log(this.todos);
+  }
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({ todo: this.newTodo, done: false });
+    }
+  }
+
   toggleTodo(index: number) {
     this.todos[index].done = !this.todos[index].done;
   }
-  
-  deleteTodo(index: number){
+
+  deleteTodo(index: number) {
     this.todos.splice(index, 1);
+  }
+
+  countOpenTodos(){
+    const done = this.todos.filter((item) => {return !item.done})
+    return done
   }
 
   title = 'my-app';
